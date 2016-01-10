bc8f9e41a49a66987a8a83d398da716c2298baa5
Allison <allisonmariebrowne@gmail.com>
2016-01-10 12:57:40 -0500

    Refactor Problem 2

--- eluer3.rb
+++ eluer3.rb
@@ -1,3 +1,19 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 #
 # What is the largest prime factor of the number 600851475143 ?
+require 'prime'
+
+primes = [];
+Prime.each(100) do |prime|
+  primes << prime  #=> 2, 3, 5, 7, 11, ...., 97
+end
+
+primes.each do |prime|
+  if Prime.prime?(prime)
+
+
+
+divided_by_two = 600851475143/2
+if Prime.prime?(divided_by_two) == false
+
+end
--- euler2.rb
+++ euler2.rb
@@ -3,42 +3,20 @@
 # 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 #
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
-
-def fibonacci(n)
-  num1 = 1,
-  num2 = 2
-  fib_numbers = []
-  fib_numbers << num1
-  fib_numbers << num2
-  i = 1
-  while i <= n
-    transfer = num2
-    num2 = num1 + num2
-    num1 = transfer
-    fib_numbers << num2
-    i = fib_numbers.last
+def fibonacci_numbers(highest_fibonacci)
+  fibonacci_sequence = [1,2]
+  fibonacci_result = []
+  loop do
+    return fibonacci_result if fibonacci_sequence[1] > 4_000_000
+    fibonacci_sequence << fibonacci_sequence.reduce(:+)
+    fibonacci_result << fibonacci_sequence[0]
+    fibonacci_sequence.shift
   end
-  if i > n
-    fib_numbers.pop
-  end
-  fib_numbers
 end
 
-
-def eliminate_odds(array)
-  even_fibs = []
-  array.each do |num|
-    if num % 2 == 0
-      even_fibs << num
-    end
-  end
-  even_fibs
+def find_evens(numbers)
+  numbers.select { |num| num.even? }
 end
 
-def sum_array(array)
-  sum = 0
-  array.each { |num| sum += num }
-  sum
-end
-
-puts sum_array(eliminate_odds(fibonacci(4_000_000)))
+evens = find_evens(fibonacci_numbers(4_000_000))
+puts evens.reduce(:+)
